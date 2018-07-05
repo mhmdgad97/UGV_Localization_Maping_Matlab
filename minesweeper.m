8070536232b2dac839b4c91c54f370f6b12f5750
mhmdgad97 <mhmdgad97@users.noreply.github.com>
2018-07-05 12:15:59 +0200

    comments on the old code writtern
    added only some comments.

--- minesweeper.m
+++ minesweeper.m
@@ -18,11 +18,11 @@ plotGrid = 'on';                 % 'off' to turn off grid
 min = 0;                         % set y-min (CM)
 max = 2000;                      % set y-max (CM)
 %%
-delay = .01;      % make sure sample faster than resolution (connot understand what he means)
+%delay = .01;      % make sure sample faster than resolution (connot understand what he means)
 %Define Function Variables
 
-time = 0;
-data = 0;
+%time = 0; %we don't need time in our plots don't copy the code then work in it. but use it to help you with your owen folw and logic
+data = 0; 
 data1 = 0;
 data2 = 0;
 count = 0;
@@ -40,11 +40,11 @@ legend(legend1,legend2,legend3)
 axis([yMin yMax min max]);
 
 grid(plotGrid);
-tic
+%tic %no need for time so no need for tic toc
 
 while ishandle(robot) %Loop when Plot is Active will run until plot is closed (dont know the function use)
-         
-         recived=fscanf(s,'%s'); %%need to make sure that (%s) works correctly
+                        %look at documentation : Description: ishandle(H) returns an array whose elements are 1 where the elements of H are graphics or Java® object handles, 
+                        %%and 0 where they are not recived=fscanf(s,'%s'); %%need to make sure that (%s) works correctly
          
          yangle=str2double(recieved(1:3));
          zangle=str2double(recieved(4:6));
@@ -67,12 +67,17 @@ while ishandle(robot) %Loop when Plot is Active will run until plot is closed (d
 %----------------------------------------------------------------------
 %to-do  the logic that will make sence of the data
 % some values (like encoder) may need pre processing
+%plese declare the robot posetion as a vector (ex:robpos[2 1])
     robXpos=cos(zangle)*encoder * cos(yangle);
     robYpos=cos(zangle)*encoder * sin(yangle);
 
     coilXpos=50;%the absolute postion of the coil relative to the centre of the robot
     coilYpos=50;
     
+%--------all the next code needs modification because you didn't consider the orientation of the robot----------
+        % ie: when the robot is going north the rigt coil pos is robpos+coilXpos
+        % while if the robot is heading south the posetion of same right
+        % coil is robos-coilXpos
     switch minestate
         case 1 % the mine is up and on the right
             
@@ -102,11 +107,20 @@ while ishandle(robot) %Loop when Plot is Active will run until plot is closed (d
             
       % break;
     end
+%--------------------------------------------------------------
 
 %----------------------------------------------------------------------
          set(robot,'XData',robXpos,'YData',robYpos);
          set(uppermines,'XData',UmineXpos,'YData',UmineYpos);
          set(lowermines,'XData',DmineXpos,'YData',DmineYpos);
+         %then after you loop once all the data is bye bye overwritten by
+         %the new data. you didn't save mines posetions you only plot them
+         %and overwite the data on the same variable (DmineXpos)
+         %this can works with the robotposetion because we don't need to save it
+         %but the mine case is different. we need to save them all in a materix
+         %as we discussed earlier this matrix is to be declared and
+         %inatialized with zeros at the beginning.
+         % try to declare all your variables at the beginning of the code
           axis([0 time(count) min max]);
           %Update the graph
           pause(delay);
