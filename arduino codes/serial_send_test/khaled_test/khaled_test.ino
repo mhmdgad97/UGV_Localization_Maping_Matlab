c0c908144c7363426ffcac0e5f81cfb6829da18e
khaled <khaledusama1998@gmail.com>
2018-07-13 13:31:17 +0200

    look at my test

--- arduino codes/serial_send_test/khaled_test/khaled_test.ino
+++ arduino codes/serial_send_test/khaled_test/khaled_test.ino
@@ -2,47 +2,65 @@
 void setup() {
     Serial.begin(9600);
 }
-
+int i=i;
 void loop() {
-  Serial.println("0000000101");
-  delay(200);
-  Serial.println("0000000101");
-  delay(2000);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0000000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-  Serial.println("0900000101");
-  delay(20);
-    
+  
+ //go straight
+      Serial.println("0000001001");
+  delay(1000); 
+      Serial.println("0000001001");
+  delay(1000); 
+      Serial.println("0000001001");
+  delay(1000); 
+      Serial.println("0000001001");
+  delay(1000); 
+      Serial.println("0000001001");
+  delay(1000); 
+      Serial.println("0000001001");
+  delay(1000);
+
+//turn right and go straigt
+      Serial.println("0900001001");
+  delay(1000); 
+      Serial.println("0900001001");
+  delay(1000); 
+      Serial.println("0900001001");
+  delay(1000); 
+      Serial.println("0900001001");
+  delay(1000); 
+      Serial.println("0900001001");
+  delay(1000); 
+      Serial.println("0900001001");
+  delay(1000); 
+
+  // //turn right and go straigt
+      Serial.println("1800001001");
+  delay(1000); 
+      Serial.println("1800001001");
+  delay(1000); 
+      Serial.println("1800001001");
+  delay(1000); 
+      Serial.println("1800001001");
+  delay(1000); 
+      Serial.println("1800001001");
+  delay(1000); 
+      Serial.println("1800001001");
+  delay(1000); 
+
+   
+//turn right and go straigt
+      Serial.println("2700001001");
+  delay(1000); 
+      Serial.println("2700001001");
+  delay(1000); 
+      Serial.println("2700001001");
+  delay(1000); 
+      Serial.println("2700001001");
+  delay(1000); 
+      Serial.println("2700001001");
+  delay(1000); 
+      Serial.println("2700001001");
+  delay(1000); 
+
+     
   }
