b0d7d99d56b888b2a7e0d8bae7efd3ed41296926
khaled <khaledusama1998@gmail.com>
2018-07-20 09:13:49 +0200

    final code
    this code uses the MPU

--- arduino codes/serial_send_test/new_demo/new_demo.ino
+++ arduino codes/serial_send_test/new_demo/new_demo.ino
@@ -1,34 +1,54 @@
 
 void setup() {
     Serial.begin(9600);
+    
 }
 int i=i;
 void loop() 
 {
-  
- //go straight
-  for(int i=0;i<5;i++)
-  {
-      Serial.println("000000-108");
-      delay(750);
-  }  
-      
-  for(int i=0;i<5;i++)
-  {
-      Serial.println("1200008");
-      delay(750);
-  }  
-      
-  for(int i=0;i<5;i++)
-  {
-      Serial.println("1800001008");
-      delay(750);
-  }  
-  
-  for(int i=0;i<5;i++)
-  {
-      Serial.println("0200001008");
-      delay(750);
-  }  
  
+      Serial.println("0000002501");
+      delay(500);
+      Serial.println("0000002500");
+      delay(500);
+      Serial.println("0000001250");delay(500);Serial.println("0000001250");
+      delay(500);
+      Serial.println("0000001250");delay(500);Serial.println("0000001250");
+      delay(500);
+      Serial.println("0000002502");
+      delay(500);
+      Serial.println("0000001250");delay(500);Serial.println("0000001250");
+      delay(500);
+      Serial.println("0000001250");delay(500);Serial.println("0000001250");
+      delay(500);
+      Serial.println("0000001250");delay(500);Serial.println("0000001250");
+      delay(500);
+      
+      Serial.println("0900001250");delay(500);Serial.println("0900001250");
+      delay(500);
+      Serial.println("0900001250");delay(500);Serial.println("0900001250");
+      delay(500);
+      
+      Serial.println("1800002503");
+      delay(500);
+      Serial.println("1800001250");delay(500);Serial.println("1800001250");
+      delay(500);
+      Serial.println("1800001250");delay(500);Serial.println("1800001250");
+      delay(500);
+      Serial.println("1800001250");delay(500);Serial.println("1800001250");
+      delay(500);
+      Serial.println("1800001250");delay(500);Serial.println("1800001250");
+      delay(500);
+      Serial.println("1800002506");
+      delay(500);
+      Serial.println("1800001250");delay(500);Serial.println("1800001250");
+      delay(500);
+      Serial.println("1800001250");delay(500);Serial.println("1800001250");
+      delay(500);
+
+      Serial.println("0900001250");delay(500);Serial.println("0900001250");
+      delay(500);
+      Serial.println("0900001250");delay(500);Serial.println("0900001250");
+      delay(500);
+      
 }
